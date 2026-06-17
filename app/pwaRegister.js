2b8617e432fd410b88b9cf1ef082874ad7da95c9
WolcenOn <fhaldon@gmail.com>
2026-06-17 17:33:38 +0100

    Register PWA service worker

---
+++ app/pwaRegister.js
@@ -0,0 +1,15 @@
+const isSecureContextForPWA = window.isSecureContext || location.hostname === "localhost";
+
+if ("serviceWorker" in navigator && isSecureContextForPWA) {
+  window.addEventListener("load", () => {
+    navigator.serviceWorker.register("./service-worker.js", { scope: "./" })
+      .then(registration => {
+        window.GestorPWA = window.GestorPWA || {};
+        window.GestorPWA.registration = registration;
+        console.info("PWA lista para uso offline", registration.scope);
+      })
+      .catch(error => {
+        console.warn("No se pudo registrar la PWA", error);
+      });
+  });
+}
