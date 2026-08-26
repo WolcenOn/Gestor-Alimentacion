f7993618c43396b029021bc57009b6c1a357a43c
WolcenOn <fhaldon@gmail.com>
2026-08-26 16:37:12 +0100

    Force service worker update checks

--- app/pwaRegister.js
+++ app/pwaRegister.js
@@ -2,10 +2,11 @@ const isSecureContextForPWA = window.isSecureContext || location.hostname === "l
 
 if ("serviceWorker" in navigator && isSecureContextForPWA) {
   window.addEventListener("load", () => {
-    navigator.serviceWorker.register("./service-worker.js", { scope: "./" })
+    navigator.serviceWorker.register("./service-worker.js", { scope: "./", updateViaCache: "none" })
       .then(registration => {
         window.GestorPWA = window.GestorPWA || {};
         window.GestorPWA.registration = registration;
+        void registration.update();
         console.info("PWA lista para uso offline", registration.scope);
       })
       .catch(error => {
